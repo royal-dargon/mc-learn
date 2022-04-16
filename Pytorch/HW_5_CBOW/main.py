271f09e00e454cf3ede8166f624297551943043a
royal-dargon <2312749625@qq.com>
2022-04-16 21:19:53 +0800

    add:add some code

--- Pytorch/HW_5_CBOW/main.py
+++ Pytorch/HW_5_CBOW/main.py
@@ -2,12 +2,15 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
+import matplotlib.pyplot as plt
 
 torch.manual_seed(1)
 
 word_to_ix = {"hello": 0, "world": 1}
+# 这个有两个参数分别是词汇量的大小和词汇量的长度
 embeds = nn.Embedding(2, 5)  # 2 words in vocab, 5 dimensional embeddings
 lookup_tensor = torch.tensor([word_to_ix["hello"]], dtype=torch.long)
+# print(lookup_tensor)
 hello_embed = embeds(lookup_tensor)
 print(hello_embed)
 
@@ -28,6 +31,7 @@ Shall sum my count, and make my old excuse,'
 Proving his beauty by succession thine!
 This were to be new made when thou art old,
 And see thy blood warm when thou feel'st it cold.""".split()
+# print("a:",test_sentence)
 # we should tokenize the input, but we will ignore that for now
 # build a list of tuples.
 # Each tuple is ([ word_i-CONTEXT_SIZE, ..., word_i-1 ], target word)
@@ -36,17 +40,21 @@ ngrams = [
         [test_sentence[i - j - 1] for j in range(CONTEXT_SIZE)],
         test_sentence[i]
     )
+    # 这次存放的是我们的目标值，通过前两个单词对后面一个单词进行预测
     for i in range(CONTEXT_SIZE, len(test_sentence))
 ]
 # Print the first 3, just so you can see what they look like.
 print(ngrams[:3])
 
 vocab = set(test_sentence)
+# 形成了类似于上面的hello world的存储方式，也就是形成了一个单词的索引
 word_to_ix = {word: i for i, word in enumerate(vocab)}
 
 
+# 语言建模器
 class NGramLanguageModeler(nn.Module):
 
+    # 这个里面存放的变量是词汇量大小和词汇的长度
     def __init__(self, vocab_size, embedding_dim, context_size):
         super(NGramLanguageModeler, self).__init__()
         self.embeddings = nn.Embedding(vocab_size, embedding_dim)
@@ -61,17 +69,41 @@ class NGramLanguageModeler(nn.Module):
         return log_probs
 
 
+
+
+
+class CBOW(nn.Module):
+
+    def __init__(self, vocab_size, embedding_dim):
+        super(CBOW, self).__init__()
+        self.embeddings = nn.Embedding(vocab_size, embedding_dim)
+        self.linear1 = nn.Linear(embedding_dim, 128)
+        self.linear2 = nn.Linear(128, vocab_size)
+
+    def forward(self, inputs):
+        embeds = sum(self.embeddings(inputs)).view(1, -1)
+        out = F.relu(self.linear1(embeds))
+        out = self.linear2(out)
+        nll_prob = F.log_softmax(out, dim=-1)
+        return nll_prob
+
+
+
+
+
+
 losses = []
 loss_function = nn.NLLLoss()
-model = NGramLanguageModeler(len(vocab), EMBEDDING_DIM, CONTEXT_SIZE)
+model = CBOW(len(vocab), EMBEDDING_DIM)
 optimizer = optim.SGD(model.parameters(), lr=0.001)
 
-for epoch in range(10):
+for epoch in range(1000):
     total_loss = 0
     for context, target in ngrams:
 
         # Step 1. Prepare the inputs to be passed to the model (i.e, turn the words
         # into integer indices and wrap them in tensors)
+        # 准备输入的信息传递给模型
         context_idxs = torch.tensor([word_to_ix[w] for w in context], dtype=torch.long)
 
         # Step 2. Recall that torch *accumulates* gradients. Before passing in a
@@ -97,7 +129,7 @@ for epoch in range(10):
 print(losses)  # The loss decreased every iteration over the training data!
 
 # To get the embedding of a particular word, e.g. "beauty"
-print(model.embeddings.weight[word_to_ix["beauty"]])
+print(model.embeddings.weight)
 
 CONTEXT_SIZE = 2  # 2 words to the left, 2 to the right
 raw_text = """We are about to study the idea of a computational process.
@@ -123,18 +155,8 @@ for i in range(CONTEXT_SIZE, len(raw_text) - CONTEXT_SIZE):
 print(data[:5])
 
 
-class CBOW(nn.Module):
-
-    def __init__(self):
-        pass
-
-    def forward(self, inputs):
-        pass
-
 # Create your model and train. Here are some functions to help you make
 # the data ready for use by your module.
-
-
 def make_context_vector(context, word_to_ix):
     idxs = [word_to_ix[w] for w in context]
     return torch.tensor(idxs, dtype=torch.long)
@@ -142,3 +164,7 @@ def make_context_vector(context, word_to_ix):
 
 # example
 make_context_vector(data[0][0], word_to_ix)
+
+plt.figure()
+plt.plot(losses)
+plt.show()
