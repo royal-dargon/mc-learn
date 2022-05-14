ea772e018e92e8b6944959a3a30f2d9a226d0aea
royal-dargon <2312749625@qq.com>
2022-05-14 17:18:13 +0800

    add:finish 1.0

--- mLearning/Bayes/main.py
+++ mLearning/Bayes/main.py
@@ -2,14 +2,7 @@ import jieba
 import pandas as pd
 import numpy as np
 import sklearn.preprocessing as sp
-
-data = pd.read_csv('result1.csv', encoding='utf-8')
-df = np.array(data)
-X = df[:, 4]
-Y = df[:, 0]
-
-lbe = sp.LabelEncoder()
-y_lable = lbe.fit_transform(Y)
+from sklearn.naive_bayes import GaussianNB, MultinomialNB
 
 
 # 读取停词表中的信息
@@ -22,28 +15,101 @@ def get_stop_list(file_name):
 
 # 这是一个使用结巴分词进行出入输入语句的函数
 def data_process(x):
-    x_list = []
-    stop_list = get_stop_list("stopwords.txt")
-    for word in x:
-        word = str(word)
-        res = ''
-        str_list = jieba.cut(word, use_paddle=True)
-        for s in str_list:
-            if s not in stop_list:
-                res += s
-        x_list.append(res)
-    return x_list
+    word_lists = []
+    for words in x:
+        res = jieba.cut(str(words), use_paddle=True)
+        word_list = list(res)
+        # print(word_list)
+        word_lists.append(word_list)
+    return word_lists
 
 
-# 这是通过处理后得到的x
-x_lists = data_process(X)
-x_list2 = []
-for x_list in x_lists:
-    l = jieba.lcut(str(x_list))
-    x_list2.append(l)
+# 这个函数是对文本的特征词进行提取, 存入的参数是之前进行分词完毕的词和题词表
+def word_dict(word_lists, stop_words):
+    # 特征列表
+    feature_dict = []
+    n = 1   # 用来计录提取的维度
+    for words in word_lists:
+        if n > 1000:
+            break
+        for word in words:
+            if word not in stop_words and not word.isdigit() and word not in feature_dict and len(word) > 1 and len(
+                    word) < 3:
+                feature_dict.append(word)
+                n += 1
+    return feature_dict
 
 
-# 构建词向量
+# 这是一个根据刚才得到的feature_dict进行向量化的操作
+def text_features(x_train, x_test, feature_word):
+    def actions(text, feature):
+        text_words = set(text)
+        feature = [1 if word in feature else 0 for word in text_words]
+        return feature
+    train_features = [actions(text, feature_word) for text in x_train]
+    test_features = [actions(text, feature_word) for text in x_test]
+    return train_features, test_features
 
 
+def text_classifer(train_feature_list, test_feature_list, train_class_list, test_class_list):
+    classifier1 = MultinomialNB().fit(train_feature_list, train_class_list)
+    print(classifier1.predict(test_feature_list))
+    print(test_class_list)
+    test_accuracy = classifier1.score(test_feature_list, test_class_list)
+    return test_accuracy
 
+
+# 解决不对齐的问题
+def solve_zero(text_features, test_features):
+    mlen = 0
+    arr_list = []
+    arr_list2 = []
+    for text_feature in text_features:
+        if mlen < len(text_feature):
+            mlen = len(text_feature)
+    for text in test_features:
+        if mlen < len(text):
+            mlen = len(text)
+    for text_feature in text_features:
+        np.array(text_feature)
+        if len(text_feature) < mlen:
+            text_feature = np.pad(text_feature, (0, mlen - len(text_feature)), 'constant', constant_values=0)
+        arr_list.append(text_feature)
+    for text in test_features:
+        np.array(text)
+        if len(text) < mlen:
+            text = np.pad(text, (0, mlen - len(text)), 'constant', constant_values=0)
+        arr_list2.append(text)
+    return np.array(arr_list), np.array(arr_list2)
+
+
+if __name__ == "__main__":
+    data = pd.read_csv('result1.csv', encoding='utf-8')
+    df = np.array(data)
+    X = df[:1000, 4]
+    Y = df[:1000, 0]
+    lbe = sp.LabelEncoder()
+    y_lable = lbe.fit_transform(Y)
+    temp = np.array([X, y_lable])
+    temp = temp.transpose()
+    # print(temp[1])
+    np.random.shuffle(temp)
+    X, y_lable = temp[:, 0], temp[:, 1]
+    # print(X)
+    x_train, y_train = X[:int(len(X) * 0.75)], y_lable[:int(len(y_lable) * 0.75)]
+    x_test, y_test = X[int(len(X) * 0.75):], y_lable[int(len(y_lable) * 0.75):]
+    # 这里是开始进行分词的处理
+    x_train = data_process(x_train)
+    x_test = data_process(x_test)
+    # 获取停词表中的信息
+    stop_words = get_stop_list("stopwords.txt")
+    feature_dict = word_dict(x_train, stop_words)
+    # print(feature_dict)
+    train_features, test_features = text_features(x_train, x_test, feature_dict)
+    # train_features, test_features = np.array(train_features), np.array(test_features)
+    train_features, test_features = solve_zero(train_features, test_features)
+    # print(train_features)
+    y_train = np.array(y_train).astype(int)
+    y_test = np.array(y_test).astype(int)
+    test_accuracy = text_classifer(train_features, test_features, y_train, y_test)
+    print(test_accuracy)
